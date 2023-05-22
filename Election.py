68ffdb53bb639ec4cd4b33550ea4b730522570ae
Ben LeBoutillier <BLeBoutillier@Dev-10.com>
2023-05-22 16:45:39 -0400

    First Version Complete

---
+++ Election.py
@@ -0,0 +1,126 @@
+
+import string, random, math, pandas as pd
+
+class Election:
+    def __init__(self, num_winners=1, ballots_df=None, num_candidates=10, num_observations=10000):
+        self.num_winners = num_winners
+        self.ballots_df = ballots_df if ballots_df else self.create_submitted_ballots(num_candidates, num_observations)
+        self.log(f'{len(self.ballots_df):,} ballots received.')
+        self.ballots_df = self.append_columns(self.ballots_df)
+
+        self.cnt_iterations = 0
+        self.results = {'Winners':[]}
+
+    def log(self, msg):
+        print(f'\n{msg}\n')
+
+    def display_ballots(self):
+        return self.ballots_df
+
+    def display_winners(self):
+        self.log(f"Winner(s): {', '.join(self.results['Winners'])}")
+
+    def create_submitted_ballots(self, num_candidates, num_observations):
+        # Create list of 'Candidate A', 'Candidate B', etc. for num_candidates.
+        names = [f'Candidate {x}' for x in string.ascii_uppercase[:num_candidates]]
+        names.reverse()
+        names.append(None)
+
+        # Create ballots that rank candidates.
+        # The rank is produced by randomly pulling candidates from the weighted list.
+        # 'None' cannot be a first choice, nor can any choices be made after 'None'.
+        ballot_results_raw = []
+        for _ in range(num_observations):
+            obs = []
+            while len(obs) < num_candidates:
+                obs_spread = names.copy()
+                random.shuffle(obs_spread)
+                if len(obs) == 0:
+                    next_choice = None
+                    while next_choice == None:
+                        next_choice = obs_spread.pop()
+                elif obs[-1] == None:
+                    next_choice = None
+                else:
+                    unacceptable = True
+                    while unacceptable:
+                        next_choice = obs_spread.pop()
+                        unacceptable = next_choice in obs
+                obs.append(next_choice)
+            ballot_results_raw.append(obs)
+
+        # Name columns as Choice_1, Choice_2, etc.
+        ballot_resutls = pd.DataFrame(data=ballot_results_raw, columns=[f'Choice_{i+1}' for i in range(len(names)-1)])
+
+        # Reset index to start at 1 instead of 0.
+        ballot_resutls.index += 1
+
+        # Rename index as 'Ballot_ID'.
+        ballot_resutls.index.rename('Ballot_ID', inplace=True)
+
+        return ballot_resutls
+        
+    def append_columns(self, df, perc_challenged=5):
+        # Create 'Is_Challenged' and 'Is_Resolved' columns.
+        #   'Is_Resolved' is 'None' if no challenge has been made.
+        df['Is_Challenged'], df['Is_Resolved'] = False, None
+
+        # Create challenges for appx. 5% of ballots.
+        # All challenged ballots are marked as resolved. These columns
+        #   are demonstrative only and don't affect the calculation.
+        for _ in range(1,math.ceil(1+len(df)*perc_challenged/100)):
+            index = random.randrange(1,len(df))
+            df.loc[index,('Is_Challenged','Is_Resolved')] = True, True
+        return df
+
+    def analyze_ballots(self):
+        df, num_winners = self.ballots_df, self.num_winners
+
+        # Drop columns not relevant to determining the winner(s).
+        df = df.drop(columns=[c for c in df.columns if not c.startswith('Choice_')], axis=1)
+        while True:
+            self.cnt_iterations += 1
+
+            # This is the magic behind SVT/RCV: Non-null values fill
+            #   toward the Choice_1 column.
+            df = df.bfill(axis=1).dropna(axis=0, how='all')
+
+            # Because values fill toward Choice_1, that is the only
+            #   column that ever needs to be tallied.
+            vc = df['Choice_1'].value_counts()
+            self.results[f'Iteration {self.cnt_iterations}'] = vc
+
+            # This voting system can produce multiple winners. A single
+            #   winner must have at least 50% of the votes, two winners 
+            #   must each have at least 33.3% of the votes, etc.
+            if vc.iloc[0] >= len(df)/(num_winners+1):
+                val = vc[vc == vc[0]].index[0]
+                self.results['Winners'].append(val)
+
+                # When a winner has emerged (by percentage of total votes),
+                #   he or she is dropped from the remaining candidates so that
+                #   the candidate's voters' second and third choices, etc. can
+                #   continue to apply.
+                df.replace(val, None, inplace=True)
+
+                # Once all the winners have emerged, the algorithm ends
+                if len(self.results['Winners']) == num_winners:
+                    self.results['Winners'].sort()
+                    self.log(f'Election analyzed in {self.cnt_iterations} iterations.')
+                    return
+            
+            # If this iteration did not produce a winner, the lowest-ranking
+            #   candidate(s) is/are removed . . . and the process repeates.
+            else:
+                for i in vc[vc == vc.min()].index:
+                    df.replace(i, None, inplace=True)
+
+    def display_full_results(self):
+        res = list(self.results.items())
+        res.reverse()
+        res.pop()
+        for x in res:
+            print(x[0])
+            for i in x[1:]:
+                for j in range(len(i)):
+                    print(f'\t{i.index[j]}\t{i[j]}')
---
+++ README.md
@@ -0,0 +1,75 @@
+# Welcome
+This repository accompanies the "Elections and Psephology" chapter of Ben "LeBoot" LeBoutillier's groundbreaking book titled [*Practical Advice for a Better World*](https://benleboutillier.com/books/Practical_Advice).
+
+# Overview
+In my book, I argue that *Ranked-Choice Voting* (RCV, or *Single Transferrable Vote* (STV)) is a fantastic voting system that must be adopted as society attempts to create a better world.
+
+### Voting Systems
+The system we currently use is called *Winner Take All* or *First Past the Post* (FPTP); in it, every voter gets one vote, and the candidate with the most votes wins. This system is fraught with problems, not the least of which is that it inevitably results in a two-party system.
+
+STV is a system in which voters *rank* the candidates on their ballots. One, two, or all candidates can be ranked. First, everyone's first choices are tallied; if a winner does not emerge, the candidate with the fewest votes is dropped, and the process repeats itself.
+
+STV can be used for one-winner or multiple-winner elections. It does not punish voting for small-party candidates, and consequently it does not force a two-party system.
+
+A more detailed discussion occurs in [*Practical Advice*](https://benleboutillier.com/books/Practical_Advice).
+
+CPG Grey uses [these short videos](https://www.cgpgrey.com/politics-in-the-animal-kingdom/) to explains the process smoothly. 
+
+### This Repository
+In [*Practical Advice for a Better World*](https://benleboutillier.com/books/Practical_Advice), I make the following claim: "With the transparency of the system, you could determine the election results yourself if you had the time and some light programming skills." This repository is intended to 1) prove that point, and 2) provide a visual for those who can't quite envision what I've described.
+
+As a professional data engineer, I don't have a good sense for what would be considered "light" programming skills, but the core of what you find here only took about an hour to write and test.
+
+**[This file](./stv.ipynb) overviews the process** and displays results. **[This file](./Election.py) is the underlying code** and makes it happen.
+
+# About
+
+### For Non-Technical People
+What you're looking at is a *GitHub repository*. This is a place for keeping and viewing code. Navigate it like you would a website or file explorer on your laptop. [This file](./stv.ipynb) is light on code, easy-to-read, and has a good overview of how simple the process can be.
+
+If you dig into [this file](./Election.py), the code can look quite intimidating. Nevertheless, I've added comments throughout to help both you and me understand it better.
+
+### For Technical People
+Everything herein is written using Python. The *Election* class (defined in [Election.py](Election.py)) contains all the properties and methods needed to run the process start to finish, as demonstrated in [this Jupyter notebook](./stv.ipynb).
+
+I used Pandas dataframes for this repo because they can be used anywhere. With potentially hundreds of millions of real-word ballots, I appreciate that spark might be a better approach.
+
+*Election's* `__init__()` method can take an existing dataframe; but by default, it will create 10,000 randomly-generated ballots for a 10-candidate election. The particulars are described using inline comments.
+
+The key to making this process work is this line of code from the `analyze_ballots()` method and uses [pandas.DataFrame.bfill](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.bfill.html):
+```python
+df = df.bfill(axis=1).dropna(axis=0, how='all')
+```
+`bfill(axis=1)` (alias for `fillna(method='bfill', axis=1)`) fills a `NaN` values with the nearest nonnull value to it's right.
+
+Because the dataframe has columns `Choice_1`, `Choice_2`, etc. from left to right, if `Choice_1` is `NaN`, then `Choice_1` is replaced with the value from `Choice_2`.
+
+Similarly, when candidates who are *not* a given voter's first choice are dropped, the next-in-line "moves up" to fill that gap. For example:
+```console
+Iteration 1:
+Ballot: (A, B, C, D, None, ...)
+
+Iteration 2; suppose Candidate C has been dropped:
+Ballot: (A, B, D, D, None, ...)
+
+Iteration 3: suppose Candidate A has been dropped:
+Ballot: (B, B, D, D, None, ...)
+
+Iteration 4: suppose Candidate B has been dropped:
+Ballot: (D, D, D, D, None, ...)
+
+Iteration 5: suppose Candidate D has been dropped:
+Ballot: (None, None, ...)
+```
+
+The analysis itself happens in a `while` loop that breaks when the needed number of winners has emerged:
+```python
+while True:
+    # Backfill df then drop entirely-NaN rows
+    # Get value counts for Choice_1
+    # Check if anyone got the necessary percentage of votes:
+        # If yes, count that person as a winner then drop from remaining options
+            # 'return' if needed number of winners has been met
+        # If no, drop candidate(s) with count == min_count
+    # (Back to top of loop)
+```
